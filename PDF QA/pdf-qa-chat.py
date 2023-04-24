c8f43c2cd2d270bd75d90802371198503c3e6a6f
Chakkaradeep (Chaks) CC <chakkaradeep@users.noreply.github.com>
2023-04-24 07:11:05 -0700

    Added code comments.

--- PDF QA/pdf-qa-chat.py
+++ PDF QA/pdf-qa-chat.py
@@ -18,12 +18,26 @@ from langchain.vectorstores import Chroma
 
 
 def preprocess(text):
+    """
+    This function preprocesses the input text by replacing newlines with spaces and removing consecutive whitespaces
+    using regex.
+    :param text:
+    :return: text
+    """
     text = text.replace('\n', ' ')
     text = re.sub('\s+', ' ', text)
     return text
 
 
 def extract_page_text(file_path: str, start_page: Optional[int] = 0, num_pages: Optional[int] = 3) -> str:
+    """
+    This function takes a PDF file path and extracts text from the specified page range.
+    It takes three parameters:
+    :param file_path:a string representing the file path of the PDF file
+    :param start_page:an optional integer representing the starting page number to extract text from. The default value is 0.
+    :param num_pages:an optional integer representing the number of pages to extract text from. The default value is 3.
+    :return:pdf text
+    """
     with pdfplumber.open(file_path) as pdf:
         pages_text = []
         for page_number in range(start_page, start_page + num_pages):
@@ -36,6 +50,14 @@ def extract_page_text(file_path: str, start_page: Optional[int] = 0, num_pages: 
 
 
 def generate_pdf_summary_chain(text: str, chain: BaseCombineDocumentsChain) -> str:
+    """
+    This function takes in a string of text and a summary chain object and generates a summary of the
+    text using the specified chain. The text is split using a RecursiveCharacterTextSplitter, then documents are
+    created from the resulting text segments. The chain is then applied to the documents to generate the summary.
+    :param text:pdf text
+    :param chain:summary chain
+    :return:concise summary of the text
+    """
     text_splitter = RecursiveCharacterTextSplitter()
     texts = text_splitter.split_text(text)
     docs = text_splitter.create_documents(texts=texts)
@@ -44,6 +66,17 @@ def generate_pdf_summary_chain(text: str, chain: BaseCombineDocumentsChain) -> s
 
 
 def generate_pdf_summary(source_path: str = "papers", output_file: str = "summaries.json"):
+    """
+    This function takes in a folder path and generates summaries of all the PDF files in that folder.
+    The summaries are stored in an output file for later retrieval. If the output file already exists, the
+    function loads and displays the existing summaries; otherwise, it generates new summaries for each
+    PDF file and stores them in the output file. The summary is generated using OpenAI's GPT-3 model
+    which is fed with a prompt template and the text extracted from the first page of each PDF.
+    The generated summaries are displayed and also saved to the output file as a JSON array of file names
+    and their corresponding summaries.
+    :param source_path: Path to the folder containing PDF files. Default value is 'papers'
+    :param output_file: Name of the output file to store the summaries. Default value is 'summaries.json'
+    """
     summaries = []
 
     prompt_template = """
@@ -91,6 +124,16 @@ def generate_pdf_summary(source_path: str = "papers", output_file: str = "summar
 
 
 def create_vectordb(vectordb_dir_path: str = "chroma_db"):
+    """
+    Creates a vector database for the PDF files in the 'papers' directory, using the Chroma library.
+
+    This function iterates through each PDF file in the directory, splits its text into smaller chunks, creates Document
+    instances from the text chunks, and extracts document embeddings using OpenAI's GPT-3 model. Then it uses Chroma to
+    store the embeddings in a vector database on disk.
+
+    :param vectordb_dir_path: The path to the directory where the vector database should be stored. If the directory
+                              does not exist, it will be created. Default value is 'chroma_db'.
+    """
     if not os.path.exists(vectordb_dir_path):
         embeddings = OpenAIEmbeddings()
 
@@ -108,6 +151,12 @@ def create_vectordb(vectordb_dir_path: str = "chroma_db"):
 
 
 def load_vectordb(vectordb_dir_path: str = "chroma_db") -> Chroma:
+    """
+    Load a Chroma vector database from the specified directory.
+
+    :param vectordb_dir_path: Directory path where the Chroma object is stored.
+    :return: A Chroma object loaded from the specified directory.
+    """
     if not os.path.exists(vectordb_dir_path):
         raise FileNotFoundError("Vector database not found. Create them first.")
     else:
@@ -118,17 +167,33 @@ def load_vectordb(vectordb_dir_path: str = "chroma_db") -> Chroma:
 
 
 def generate_pdf_embeddings(vectordb_dir_path: str = "chromadb") -> Chroma:
+    """
+    This function generates Chroma vector database for PDF documents in the specified directory to store
+    OpenAI embeddings. If the Chroma database does not exist, it will be created first.
+    :param vectordb_dir_path: directory path for the Chroma database. Default value is 'chromadb'
+    :return: Chroma object with the PDF embeddings
+    """
     if not os.path.exists(vectordb_dir_path):
         create_vectordb()
     return load_vectordb()
 
 
 def query_dataset_retrieval(query: str, qa_chain: BaseRetrievalQA):
+    """
+    Execute the RetrievalQA chain
+    :param query: Query to execute
+    :param qa_chain: The RetrievalQA chain object
+    """
     response = qa_chain.run(query)
     return response
 
 
 def process_input(user_input: str, vectordb: Chroma):
+    """
+    Get input from the user and perform a Retrieval QA on the PDF documents which are stored as embeddings.
+    :param user_input: Input text from the user
+    :param vectordb: Chroma vector database
+    """
     prompt_template = """
         Use the following pieces of context to answer the question at the end.\n 
         If you don't know the answer, just say that you don't know, don't try to make up an answer.\n
@@ -163,8 +228,11 @@ def process_input(user_input: str, vectordb: Chroma):
         return error_message
 
 
-# Get user input and process it
 def get_user_input(vectordb: Chroma):
+    """
+    Get user input and process it
+    :param vectordb: Chroma vector database
+    """
     while True:
         user_input = input(f"\nYour question: ")
         if user_input.lower() == 'exit':
@@ -176,6 +244,9 @@ def get_user_input(vectordb: Chroma):
 
 
 def main():
+    """
+    Our main function
+    """
     generate_pdf_summary()
     vectordb = generate_pdf_embeddings()
     get_user_input(vectordb)
@@ -185,4 +256,5 @@ if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('config.ini')
     os.environ["OPENAI_API_KEY"] = config.get('API_KEYS', 'OPENAI_API_KEY')
+
     main()
